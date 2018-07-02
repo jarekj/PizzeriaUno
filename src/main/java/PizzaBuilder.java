b0ff7fbf8a1a9ffaf5f4b5456d34eb3fdad1d501
Jarek <jjankowski1@gmail.com>
2018-07-02 20:24:40 +0200

    add abstract PizzaBuilder class

---
+++ src/main/java/PizzaBuilder.java
@@ -0,0 +1,9 @@
+public abstract class PizzaBuilder {
+    protected Pizza pizza;
+
+    public abstract void buildDough();
+
+    public abstract void buildTopping();
+
+    public abstract void buildSauce();
+}
