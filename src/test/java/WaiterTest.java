9e1143bfd9272af0adc37927f5dc2d11d6ea7720
Jarek <jjankowski1@gmail.com>
2018-07-03 19:34:17 +0200

    add test for pepperoni and hawaiian pizza

--- src/test/java/WaiterTest.java
+++ src/test/java/WaiterTest.java
@@ -5,14 +5,34 @@ public class WaiterTest {
     @Test
     public void givenWaiterWhenSetMargharitaPizzaShouldReturnMargharitaPizza() {
         Waiter waiter = new Waiter();
+
         waiter.setPizzaBuilder(new MargheritaBuilder());
         Pizza newPizza = waiter.getPizza();
+
         String actualDough = newPizza.getDough();
         String actualSaouce = newPizza.getSaouce();
         String actualTopping = newPizza.getTopping();
         String expectedDough = "grube";
         String expectedSaouce = "pomidorowy";
         String expectedTopping = "mozarella";
+
+        Assert.assertEquals(expectedDough, actualDough);
+        Assert.assertEquals(expectedSaouce, actualSaouce);
+        Assert.assertEquals(expectedTopping, actualTopping);
+    }
+    @Test
+    public void givenWaiterWhenSetPepperoniPizzaShouldReturnPepperoniPizza() {
+        Waiter waiter = new Waiter();
+
+        waiter.setPizzaBuilder(new PepperoniBuilder());
+        Pizza newPizza = waiter.getPizza();
+
+        String actualDough = newPizza.getDough();
+        String actualSaouce = newPizza.getSaouce();
+        String actualTopping = newPizza.getTopping();
+        String expectedDough = "cienkie";
+        String expectedSaouce = "pomidorowy z ziołami";
+        String expectedTopping = "mozarell, peperoni";
         Assert.assertEquals(expectedDough, actualDough);
         Assert.assertEquals(expectedSaouce, actualSaouce);
         Assert.assertEquals(expectedTopping, actualTopping);
